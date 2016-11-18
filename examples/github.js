c629012dfd6cfacdc273dea530c25f4165d13c27
Rikkert Koppes <rikkert@rikkertkoppes.com>
2016-11-18 20:59:41 +0100

    trim text after resolving, it may become an option, added github example

---
+++ examples/github.js
@@ -0,0 +1,15 @@
+var request = require('request');
+var scrapio = require('../lib/scrapio.js');
+
+request.get('https://github.com/rikkertkoppes/scrapio', function(err, response, body) {
+    var res = scrapio.load(body).tmpl({
+    	title: scrapio('title').text(),
+    	files: scrapio('table.files tr').tmpl({
+    		name: scrapio('td:nth-child(2)').text()
+    	}),
+    	branches: scrapio('.branch-select-menu button + .select-menu-modal-holder .select-menu-item-text').tmpl(
+    		scrapio().text()
+    	)
+    });
+    console.log(res);
+});
