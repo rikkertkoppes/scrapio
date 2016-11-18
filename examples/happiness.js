5311f045d6163d8bd93ee1f8751d1d741eafa26f
Rikkert Koppes <rikkert@rikkertkoppes.com>
2016-11-18 20:46:33 +0100

    formatting

--- examples/happiness.js
+++ examples/happiness.js
@@ -2,12 +2,12 @@ var request = require('request');
 var scrapio = require('../lib/scrapio.js');
 
 request.get('https://en.wikipedia.org/wiki/World_Happiness_Report', function(err, response, body) {
-	var res = scrapio.load(body).tmpl(
-		scrapio('.wikitable tr').tmpl({
-			rank: scrapio('td:nth-child(1)').text(),
-			name: scrapio('td:nth-child(2)').text(),
-			score: scrapio('td:nth-child(3)').text(),
-		})
-	);
-	console.log(res);
-})
+    var res = scrapio.load(body).tmpl(
+        scrapio('.wikitable tr').tmpl({
+            rank: scrapio('td:nth-child(1)').text(),
+            name: scrapio('td:nth-child(2)').text(),
+            score: scrapio('td:nth-child(3)').text(),
+        })
+    );
+    console.log(res);
+});
